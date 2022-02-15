5fa2576697fa87ff8db2442589b5684a6deb7667
TamoorW <waheedtamoor@gmail.com>
2022-02-15 15:16:39 +0000

    Made schema for user details

---
+++ schema.js
@@ -0,0 +1,20 @@
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+
+try {
+  await Deno.remove("users.db");
+} catch {
+  // nothing to remove
+}
+
+const db = new DB("./users.db");
+
+await db.query(
+  `CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    encrypted_password TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    admin BOOLEAN
+  )`
+);
